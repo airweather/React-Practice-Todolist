b7ec16a69498087175c867b15e9c115c7cddff63
Jun <wnsghd1@naver.com>
2023-01-11 01:21:52 +0900

    완료된 todo 삭제기능 구현

--- src/component/Todolist.js
+++ src/component/Todolist.js
@@ -6,13 +6,29 @@ export default function Todolist() {
   const [todos, setTodos] = useState([]);
 
   let text = '';
-  const complete = () => {
+  let id = todos.length;
+
+  const complete = (props) => {
+    const num = props;
+    const newTodos = [];
+
+    const complete = window.confirm('완료했나요?');
+
+    if(complete) {
+      todos.map((todo) => {
+        if(todo.id !== num) {
+          newTodos.push(todo);
+        }
+      })
+    }
+    setTodos(newTodos);
     
   };
   
   const push = () => {
     if(text ==='') return;
-    setTodos([...todos, text])
+    setTodos([...todos, {id, text}])
+    id++;
   };
 
   const textInput = (e) => {
@@ -41,16 +57,17 @@ export default function Todolist() {
     )
   }
 
+  let num;
   const InputList = () => {
     return (
       <div>
-        {todos.map((todo, i) => 
-          <Grid container sx={{m:3}} key={i}>
+        {todos.map((todo) => 
+          <Grid container sx={{m:3}} key={todo.id}>
             <Grid item xs>
-              <Typography variant="h5" component="h5" sx={{m:1}}>{todo}</Typography>
+              <Typography variant="h5" component="h5" sx={{m:1}}>{todo.text}</Typography>
             </Grid>
             <Grid item>
-              <Button variant="contained" sx={{m:1 ,mr:5}} color="success" onClick={complete}>완료</Button>
+              <Button variant="contained" sx={{m:1 ,mr:5}} color="success" onClick={()=>complete(todo.id)}>완료</Button>
             </Grid>
           </Grid>
         )}
