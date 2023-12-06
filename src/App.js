33d36c1e98f005bbcdb9606e7bc784039b7c2dca
Jun <wnsghd1@naver.com>
2023-12-06 16:25:39 +0900

    Container

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import './App.css';
 import Todolist from './component/Todolist';
 // import Container from '@mui/material/Container';
 import Calendar from './component/Calendar';
+import Container from './component/Container';
 // import Todoinput from './component/Todoinput';s
 
 function App() {
   return (
     <>
+      <Container />
       <Calendar/>
     </>
   );
---
+++ src/component/Container.jsx
@@ -0,0 +1,11 @@
+const Container = () => {
+  return (
+    <>
+      <h1>
+        Container
+      </h1>
+    </>
+  )
+}
+
+export default Container;
