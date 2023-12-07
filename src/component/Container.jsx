3cabe71c8541fe2586e507075cfce3c30eb09f97
Jun <wnsghd1@naver.com>
2023-12-07 17:48:26 +0900

    Container

--- src/component/Container.jsx
+++ src/component/Container.jsx
@@ -1,9 +1,15 @@
+import Calendar from './Calendar';
+import Todolist from './Todolist';
+
+
 const Container = () => {
   return (
     <>
       <h1>
         Container
       </h1>
+      <Calendar />
+      <Todolist />
     </>
   )
 }
