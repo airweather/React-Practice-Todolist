e41a542eb37da82613907e20e6cdd391cfe082e6
Jun <wnsghd1@naver.com>
2023-12-15 23:19:39 +0900

    monthly todo list

--- src/component/Container.jsx
+++ src/component/Container.jsx
@@ -5,22 +5,46 @@ import {useState} from 'react';
 const Container = () => {
   const today = new Date();
 
-  const [todoLists, setTodoLists] = useState({});
   const [targetDate, setTargetDate] = useState(today);
   const [selectedDate, setSelectedDate] = useState(today);
-
+  const [todoLists, setTodoLists] = useState([]);
+  
   const dateNormalization = (date) => {
     return new Date(date.getFullYear(), date.getMonth(), date.getDate(), 0, 0, 0, 0);
   }
+
+  const monthlyTodoList = (date) => {
+    const firstDate = new Date(date.getFullYear(), date.getMonth()+1, 1);
+    const lastDate = new Date(date.getFullYear(), date.getMonth()+1, 0).getDate();
+
+    const todoListData = [];
+
+    for(let i = 1; i <= lastDate; i++) {
+      const todoDate = new Date(date.getFullYear(), date.getMonth(), i);
+
+      todoListData.push(
+        {
+          date:todoDate,
+          list:[]
+        }
+      )
+    }
+
+    return todoListData;
+  
+  }
+
 
   return (
     <>
       <h1>
         Container
       </h1>
-      <div>{dateNormalization(selectedDate).getFullYear()} {dateNormalization(selectedDate).getMonth()+1} {dateNormalization(selectedDate).getDate()}</div>
-      <Calendar targetDate={targetDate} setSelectedDate={setSelectedDate} todoLists={todoLists}/>
-      <Todolist date={dateNormalization(selectedDate)} todoLists={todoLists} setTodoLists={setTodoLists}/>
+      <div>
+        <div>{dateNormalization(selectedDate).getFullYear()} {dateNormalization(selectedDate).getMonth()+1} {dateNormalization(selectedDate).getDate()}</div>
+        <Calendar targetDate={targetDate} setSelectedDate={setSelectedDate} todoLists={todoLists}/>
+        <Todolist date={dateNormalization(selectedDate)} todoLists={todoLists} setTodoLists={setTodoLists}/>
+      </div>
       </>
   )
 }
